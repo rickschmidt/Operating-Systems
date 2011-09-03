a98bc434e7c5d1bcb4fd6e4facb837d6a47372b0
Rick Schmidt <rick@rickschmidt.org>
2011-09-03 13:44:57 -0500

    Comp ran out of all storage and failed, redo."

--- hw1/entry.c
+++ hw1/entry.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+#include <iostream>
+
+ 
+
+struct Nodes{
+		char *data; //Holds info for node
+		std::vector<Nodes> children;
+		
+
+		};
+		
+		
 int main(int argc, char* argv[]) {
 	FILE *fp;
 	char *token;
   	char line[100];			//Will hold onto string, each line from stream gets
 	fp = popen("ps -ax -o command,pid,ppid", "r");
 	char *root;
+
+// 	typedef std::vector<char*> nodes;
 	
-	typedef struct Node{
-		char *data; //Holds info for node
-		Node *left;
-		Node *right;
-		}Node;
+		
+	// typedef struct Nodes *NODEPTR;	
+	typedef std::vector<Nodes>tree;	
+		
+		tree myTree;
+		Nodes mynodes;
+		Nodes secondnode;
+		Nodes thirdnode;
+		mynodes.data="hello";
+		secondnode.data="world";
+		thirdnode.data="!!!";
+		mynodes.children.push_back(secondnode);
+		mynodes.children.push_back(thirdnode);
+		
+		
+		printf("The first node has a value of %s and contains a node with the value of %s, oh and %s\n",mynodes.data,mynodes.children.front().data, mynodes.children.back().data);
+ 
+
+		// printf("mynodes.data %s, and data of next is %s",mynodes.data,mynodes.children->front);
+		 myTree.push_back(mynodes);
+		
 
 	 while ( fgets( line, sizeof line, fp)){  //Stream gets, grabs from stream, stores in line, grabs until \n reached
 		token=strtok(line,"\t");
--- hw1/entry.cpp
+++ hw1/entry.cpp
@@ -6,7 +6,9 @@
  
 
 struct Nodes{
-		char *data; //Holds info for node
+		char *cmd; //Holds info for node
+		char *pid;
+		char *ppid;
 		std::vector<Nodes> children;
 		
 
@@ -17,8 +19,8 @@ int main(int argc, char* argv[]) {
 	FILE *fp;
 	char *token;
   	char line[100];			//Will hold onto string, each line from stream gets
-	fp = popen("ps -ax -o command,pid,ppid", "r");
-	char *root;
+	fp = popen("ps -ax -c -o uid,uid,uid,uid,uid,uid,pid,ppid,uid,uid,uid,command", "r");
+
 
 // 	typedef std::vector<char*> nodes;
 	
@@ -26,50 +28,74 @@ int main(int argc, char* argv[]) {
 	// typedef struct Nodes *NODEPTR;	
 	typedef std::vector<Nodes>tree;	
 		
-		tree myTree;
-		Nodes mynodes;
-		Nodes secondnode;
-		Nodes thirdnode;
-		mynodes.data="hello";
-		secondnode.data="world";
-		thirdnode.data="!!!";
-		mynodes.children.push_back(secondnode);
-		mynodes.children.push_back(thirdnode);
+		// tree myTree;
+		// 		Nodes mynodes;
+		// 		Nodes secondnode;
+		// 		Nodes thirdnode;
+		// mynodes.cmd="hello";
+		// secondnode.cmd="world";
+		// thirdnode.cmd="!!!";
+		// mynodes.children.push_back(secondnode);
+		// mynodes.children.push_back(thirdnode);
 		
-		
-		printf("The first node has a value of %s and contains a node with the value of %s, oh and %s\n",mynodes.data,mynodes.children.front().data, mynodes.children.back().data);
+		char cmd[20];
+		char pid[20];
+		char ppid[20];
+		char zero[20]="0";
+		char command[8]="COMMAND";
+		// printf("The first node has a value of %s and contains a node with the value of %s, oh and %s\n",mynodes.data,mynodes.children.front().data, mynodes.children.back().data);
  
 
 		// printf("mynodes.data %s, and data of next is %s",mynodes.data,mynodes.children->front);
-		 myTree.push_back(mynodes);
+		 // myTree.push_back(mynodes);
 		
-
+		Nodes root;
 	 while ( fgets( line, sizeof line, fp)){  //Stream gets, grabs from stream, stores in line, grabs until \n reached
-		token=strtok(line,"\t");
 	
-		if(strstr(token,"COMMAND            PID  PPID")!=NULL){
-							//This is silly but it removes the header for now
-							// token=strtok(NULL,"\t");
-				//			printf("Token: %s",token);
-				
-						}
+		sscanf(line,"%*s %*s %*s %*s %*s %*s %s %s %*s %*s %*s %s",pid,ppid,cmd);
 	
-		if(strstr(token,"/sbin/launchd        1     0")!=NULL){
-			root=token;
-			printf("The root is %s",root);
-			token=strtok(NULL,"\t");
+			//printf("The cmd is %s the pid is %s and the ppid is %s\n",cmd,pid,ppid);
+	
+	
+		//Ignore header
+		if(strcmp(ppid,command)==0){
+			//Go back to while
+		}
+		//Root
+		if(strcmp(ppid,zero)==0){
+			root.cmd=cmd;
+			root.pid=pid;
+			root.ppid=ppid;
+			printf("The root command is %s, the pid is %s and ppid is %s\n",cmd,pid,ppid);
 		}
 	
-		while(token!=NULL){
-			if(strstr(token,"COMMAND            PID  PPID")!=NULL){
-					//This is silly but it removes the header for now
-					 token=strtok(NULL,"\t");
-							//printf("Token: %s",token);
-				
-						}
-			printf("TOKEN: %s",token);
-			token=strtok(NULL,"\t");
-		}
+	
+	
+	
+	
+		// if(strstr(token,"COMMAND            PID  PPID")!=NULL){
+		// 							//This is silly but it removes the header for now
+		// 							// token=strtok(NULL,"\t");
+		// 				//			printf("Token: %s",token);
+		// 				
+		// 						}
+		// 	
+		// 		if(strstr(token,"/sbin/launchd        1     0")!=NULL){
+		// 			root=token;
+		// 			printf("The root is %s",root);
+		// 			token=strtok(NULL,"\t");
+		// 		}
+		// 	
+		// 		while(token!=NULL){
+		// 			if(strstr(token,"COMMAND            PID  PPID")!=NULL){
+		// 					//This is silly but it removes the header for now
+		// 					 token=strtok(NULL,"\t");
+		// 							//printf("Token: %s",token);
+		// 				
+		// 						}
+		// 			printf("TOKEN: %s",token);
+		// 			token=strtok(NULL,"\t");
+		// 		}
 	  }
 
 	return 0;
