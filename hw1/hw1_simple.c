a3b7d8ccbbfbeaf76c27edc2d051721d2415e4ad
Big Brother <root@192.168.59.129>
2011-09-06 22:26:28 +0000

    more from minix

--- hw1/hw1_simple.c
+++ hw1/hw1_simple.c
@@ -16,12 +16,14 @@ int main(){
 	char c[200]="CMD";
 	char extra[200];
 	char extra2[200];
+	char extra3[200];
 
      /* we ingore the return value of t_insert */
 	 while ( fgets( line, sizeof line, fp)){  //Stream gets, grabs from stream, stores in line, grabs until \n reached
 
-		sscanf(line,"%*s %*s %*s %s %d %*s %*s %s %*s %s %s",pid,&ppid,extra,extra2,cmd);
+		sscanf(line,"%*s %*s %*s %s %d %*s %*s  %s %*s %s  %s",pid,&ppid,extra,extra2,cmd);
 		int i;
+		printf("%s,%d,%s,%s,%s\n",pid,&ppid,extra,extra2,cmd);
 	
 
 	if(strcmp(cmd,c)==0){
@@ -33,8 +35,8 @@ int main(){
 		for(i=0;i<(ppid);i++){
 			printf("-\t");
 		}
-		if(strncmp(extra," ",1)==0){
-			printf("%s,%s\n",extra2,pid);
+		if(strncmp(extra,"",1)==0){
+			printf("!%s,%s\n",extra2,pid);
 		}
 		else{
 			printf("%s,%s\n",cmd,pid);
