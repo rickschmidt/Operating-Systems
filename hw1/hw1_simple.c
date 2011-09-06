7da5f777034e1d6e10358c50ac637e31a026149c
Big Brother <root@192.168.59.129>
2011-09-06 21:14:01 +0000

    Work form minix

--- hw1/hw1_simple.c
+++ hw1/hw1_simple.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main(){
       /* construct tree by hand */
 	FILE *fp;
 
   	char line[100];			//Will hold onto string, each line from stream gets
-	fp = popen("ps -ax -c -o uid,uid,uid,uid,uid,uid,pid,ppid,uid,uid,uid,command", "r");
+	fp = popen("ps -axl", "r");
 
     //  struct tree_node *tp, *root_p = 0;
 
 	char cmd[200];
 	char pid[200];
 	int ppid;
-	
+	char c[200]="CMD";
+	char extra[200];
+	char extra2[200];
 
      /* we ingore the return value of t_insert */
 	 while ( fgets( line, sizeof line, fp)){  //Stream gets, grabs from stream, stores in line, grabs until \n reached
 
-		sscanf(line,"%*s %*s %*i %*s %*s %*s %s %d %*s %*s %*s %s",pid,&ppid,cmd);
+		sscanf(line,"%*s %*s %*s %s %d %*s %*s %s %*s %s %s",pid,&ppid,extra,extra2,cmd);
 		int i;
-		
+	
 
-
-		for(i=0;i<(ppid/10);i++){
-			printf("\t");
+	if(strcmp(cmd,c)==0){
+		  return(0);  
 		}
-		printf("%s, %s\n",cmd,pid);
+		else{	
+
+
+		for(i=0;i<(ppid);i++){
+			printf("-\t");
+		}
+		if(strncmp(extra," ",1)==0){
+			printf("%s,%s\n",extra2,pid);
+		}
+		else{
+			printf("%s,%s\n",cmd,pid);
+		}
+		}
 	
 		
 	}
