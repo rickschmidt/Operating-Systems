0acc71ef4a6dbb2847ddc91d62930016c2ecac24
Rick Schmidt <rick@rickschmidt.org>
2011-09-21 15:59:15 -0500

    Working on hw2

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -1,32 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include  <sys/types.h>
+
+// int addToArray(char listofpids[]){
+// 		char *listofpids2;
+// 		int sizeOfList;
+// 		sizeOfList=(sizeof(*listofpids)/8);
+// 	listofpids2=realloc(*listofpids,sizeOfList+1);
+// 	printf("size of listofpids2: %zu\n",sizeof(listofpids2)/8);
+// 	return 1;
+// }
 
 int main(int argc, char* argv[]) { 
 
 printf("Cout is : %d\n",argc);
 int i;
-   for(i=0; i < argc; i++) printf("argv is: %s\n", argv[i]);
 
-char sleep[100]="sleep";
+//print all of the varargs for testing...
+for(i=0; i < argc; i++) printf("argv %d is: %s\t",i, argv[i]); printf("\n");
+
+
+char sleep1[100]="sleep";
 char list[10]="list";
-char wait[10]="wait";
+char wait1[10]="wait";
 char pid[10]="PID";
 char pid_list[10]="PID-list";
 char time[10]="time";
 char kill[10]="kill";
-pid_t pid1;
+pid_t pid1=0;
 pid_t ppid1;
+pid_t pid2;
+pid_t pidholder;
+int status;
+int sizeOfList;
+char *listofpids[2];
+
+
 ppid1=getppid();
 printf("ppid1 is %d\n",ppid1);
-if(strcmp(argv[1],sleep)==0){
+printf("size of array: %zu\n",sizeof(listofpids)/8);
+sizeOfList=(sizeof(listofpids)/8);
+
+if(strcmp(argv[1],sleep1)==0){
 	printf("sleeping");
 	printf(" for %s\n",argv[2]);
-	fork();
-	pid1=getpid();
+	printf("pid1 is %d\n",pid1);
+
+	pidholder=fork();
+	// addToArray(&listofpids);
+	switch(pidholder){
+		case(0):
+			printf("\tChild prcoess created\n");
+			int n=atoi(argv[2]);
+			sleep(n);
+			int s;
+			s=wait(&pid1);
+			printf("\tchild pid2 is %d\n",pid2);
+			printf("\tchilds is %d\n", s);
+			break;
+		case(-1):
+			printf("Error child not created\n");
+			break;
+		
+	}
 	
 	
-	printf("pid1 is %d",pid1);
+	printf("pid1b is %d\n",pid1);
 }
 
 if(strcmp(argv[1],list)==0){
@@ -34,7 +74,7 @@ if(strcmp(argv[1],list)==0){
 
 }
 
-if(strcmp(argv[1],wait)==0){
+if(strcmp(argv[1],wait1)==0){
 	if(strcmp(argv[2],pid)==0){
 		printf("wait PID\n");
 	}
