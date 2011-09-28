6945be0099bd0e21debc9013cbb73854b9597030
Rick Schmidt <rick@rickschmidt.org>
2011-09-28 12:57:55 -0500

    Added pipes to hw2

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -2,112 +2,124 @@
 #include <stdlib.h>
 #include <string.h>
 #include  <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
 
-// int addToArray(char listofpids[]){
-// 		char *listofpids2;
-// 		int sizeOfList;
-// 		sizeOfList=(sizeof(*listofpids)/8);
-// 	listofpids2=realloc(*listofpids,sizeOfList+1);
-// 	printf("size of listofpids2: %zu\n",sizeof(listofpids2)/8);
-// 	return 1;
-// }
 
+void ignore(){
+	
+}
 int main(int argc, char* argv[]) { 
 
-printf("Cout is : %d\n",argc);
-int i;
+	printf("Cout is : %d\n",argc);
+	int i;
 
-//print all of the varargs for testing...
-for(i=0; i < argc; i++) printf("argv %d is: %s\t",i, argv[i]); printf("\n");
-
-
-char sleep1[100]="sleep";
-char list[10]="list";
-char wait1[10]="wait";
-char pid[10]="PID";
-char pid_list[10]="PID-list";
-char time[10]="time";
-char kill[10]="kill";
-pid_t pid1=0;
-pid_t ppid1;
-pid_t pid2;
-pid_t pidholder;
-int status;
-int sizeOfList;
-char *listofpids[2];
+	//print all of the varargs for testing...
+	for(i=0; i < argc; i++) printf("argv %d is: %s\t",i, argv[i]); printf("\n");
 
 
-ppid1=getppid();
-printf("ppid1 is %d\n",ppid1);
-printf("size of array: %zu\n",sizeof(listofpids)/8);
-sizeOfList=(sizeof(listofpids)/8);
+	char sleep1[100]="sleep";
+	char list[10]="list";
+	char wait1[10]="wait";
+	char pid[10]="PID";
+	char pid_list[10]="PID-list";
+	char time[10]="time";
+	char kill[10]="kill";
+	pid_t pid1=0;
+	pid_t ppid1;
+	pid_t pid2;
+	pid_t pidholder;
+	int status;
+	int sizeOfList;
+	int listofpids[100];
+	int *listposition;
 
-if(strcmp(argv[1],sleep1)==0){
-	printf("sleeping");
-	printf(" for %s\n",argv[2]);
-	printf("pid1 is %d\n",pid1);
+	// ppid1=getppid();
+	// printf("ppid1 is %d\n",ppid1);
+	// printf("size of array: %zu\n",sizeof(listofpids)/8);
+	// sizeOfList=(sizeof(listofpids)/8);
+	
+	if(strcmp(argv[1],sleep1)==0){
+		printf("sleeping");
+		printf(" for %s\n",argv[2]);
+		printf("pid1 is %d\n",pid1);
 
-	pidholder=fork();
-	// addToArray(&listofpids);
-	switch(pidholder){
-		case(0):
-			printf("\tChild prcoess created\n");
-			int n=atoi(argv[2]);
-			sleep(n);
-			int s;
-			s=wait(&pid1);
-			printf("\tchild pid2 is %d\n",pid2);
-			printf("\tchilds is %d\n", s);
-			break;
-		case(-1):
-			printf("Error child not created\n");
-			break;
-		
-	}
+		signal(SIGIOT,SIG_IGN);
+		pidholder=fork();
+		 // pidholder=listofpids[listposition];
+		printf("pidholder1 %d\n",pidholder);
+	
+		if(pidholder==0){
+				printf("pidholder %d",pidholder);
+	
+				printf("\tChild prcoess created\n");
+				int n=atoi(argv[2]);
+				sleep(n);
+				int s;
+				s=wait(&pid1);
+				printf("\tchild pid2 is %d\n",pid2);
+				printf("\tchilds is %d\n", s);
+			}
+		else{
+			int pipe;
+				pipe = open("pids", O_WRONLY);
+				write(pipe, pidholder, sizeof(pidholder));
+				
+					printf("listofpids in else:  %d\n",listofpids[0]);
+			
+	
+		}
 	
 	
-	printf("pid1b is %d\n",pid1);
-}
-
-if(strcmp(argv[1],list)==0){
-	printf("list");
-
-}
-
-if(strcmp(argv[1],wait1)==0){
-	if(strcmp(argv[2],pid)==0){
-		printf("wait PID\n");
-	}
 	
-	if(strcmp(argv[2],pid_list)==0){
-		printf("wait PID-list");
+		// exit(0);
 	}
+
+	if(strcmp(argv[1],list)==0){
+		printf("list");
+		int pipe;
+		pipe=open("pids",  O_RDONLY);
+		read(pipe,listofpids,sizeof(listofpids));
+		// close(pipe);
+		printf("listofpids in list:  %d\n",listofpids[0]);
+		wait(10);
+		// exit(0);
+	}
+
+	if(strcmp(argv[1],wait1)==0){
+		if(strcmp(argv[2],pid)==0){
+			printf("wait PID\n");
+		}
+	
+		if(strcmp(argv[2],pid_list)==0){
+			printf("wait PID-list");
+		}
 	
 
-}
-
-if(strcmp(argv[1],pid)==0){
-	printf("pid");
-
-}
-if(strcmp(argv[1],time)==0){
-	if(strcmp(argv[2],pid)==0){
-		printf("time PID");
 	}
 
-}
+	if(strcmp(argv[1],pid)==0){
+		printf("pid");
 
-if(strcmp(argv[1],kill)==0){
-	if(strcmp(argv[2],pid)==0){
-		printf("kill PID");
+	}
+	if(strcmp(argv[1],time)==0){
+		if(strcmp(argv[2],pid)==0){
+			printf("time PID");
+		}
+
 	}
 
-}
+	if(strcmp(argv[1],kill)==0){
+		if(strcmp(argv[2],pid)==0){
+			printf("kill PID");
+		}
+
+	}
+
+	printf("none matched");
 
 
 
 
-
-
-	return(0);
+		return(0);
 }
