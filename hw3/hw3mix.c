d64fddb5c70dcad452f15e3e907dbed2601d2955
Rick Schmidt <rick@rickschmidt.org>
2011-10-22 11:45:41 -0500

    Added mix program that takes the previous two and executues both

---
+++ hw3/hw3mix.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void cpu_process(float cpu_factor);
+void io_process(float io_factor);
+int main(int argc, char* argv[]){
+	float cpu_factor;
+	cpu_factor=atof(argv[1]);
+	float io_factor;
+	io_factor=atof(argv[2]);
+	pid_t pidholder;
+	pidholder=fork();
+	
+	
+		if(pidholder==0){
+			//Child Process
+			cpu_process(cpu_factor);
+
+		}
+		else{
+			//Parent Process
+			io_process(io_factor);
+		}
+
+}
+
+
+void cpu_process(float cpu_factor){
+	float i=1;
+	float j;
+
+
+	time_t start,end;
+	time(&start);
+	printf("Starting CPU..\n");
+	while(i<(100000*cpu_factor)){
+		for(j=0;j<i;j++){
+			i/j;
+		}
+		i++;
+	}
+	time(&end);
+	double diff;
+	diff=difftime(end,start);
+	printf("Delta Time CPU: %.21f\n",diff);
+}
+
+void io_process(float io_factor){
+	time_t start,end;
+	time(&start);
+	
+	printf("Starting IO..\n");
+	FILE *fp;
+	float i;
+	float j;
+	while(i<(100000*io_factor)){
+		fp = fopen ("/tmp/hw3.txt","w");
+		
+			fprintf(fp,"%s\n","This is a io bound process"); /*writes*/ 
+		
+		fclose(fp);
+		i++;
+	}
+	time(&end);
+	double diff;
+	diff=difftime(end,start);
+	printf("Delta Time IO: %.21f\n",diff);
+
+}
