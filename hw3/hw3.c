dcb3bf8cbe3ad674918cbf4413b804c3b92c11e8
Big Brother <root@192.168.59.130>
2011-10-22 10:13:06 +0000

    Added timestamps

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char* argv[]){
 	float i=1;
 	float j;
-	while(i<200000){
+	time_t start,end;
+	time(&start);
+	while(i<100000){
 		for(j=0;j<i;j++){
 			i/j;
 		}
 		i++;
 	}
+	time(&end);
+	double diff;
+	diff=difftime(end,start);
+	printf("Time: %.21f\n",diff);
 	exit(0);
 }
