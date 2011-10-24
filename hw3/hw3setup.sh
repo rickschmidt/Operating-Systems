4073e71833030519379e8af437d345a058520408
Rick Schmidt <rick@rickschmidt.org>
2011-10-24 13:16:48 -0500

    Added csv data and started R analysis

--- hw3/hw3setup.sh
+++ hw3/hw3setup.sh
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/bash
 START=$(date +%s)
 ./hw3io 2 &
-./hw3cpu 2 &
+./hw3cpu 1 &
 ./hw3mix 1 2 &
 wait
 END=$(date +%s)
---
+++ hw3/hw3stats.R
@@ -0,0 +1,13 @@
+zz<-read.csv("hw3stats.csv",head=TRUE,sep=",")
+
+y1<-zz[c(1:10),]
+y2<-zz[c(13:22),]
+y3<-zz[c(25:34),]
+y4<-zz[c(37:46),]
+y5<-zz[c(49:58),]
+
+print(y1)
+print(y2)
+print(y3)
+print(y4)
+print(y5)
