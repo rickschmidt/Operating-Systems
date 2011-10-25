aebcfa854689b51c1e9e32a9e4bffb061c6890bc
Rick Schmidt <rick@rickschmidt.org>
2011-10-25 17:32:48 -0500

    Still going back to perfect hw2

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -4,6 +4,7 @@
 #include  <sys/types.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <sys/wait.h>
 
 
 void ignore(){
@@ -34,63 +35,71 @@ int main(int argc, char* argv[]) {
 	int listofpids[100];
 	int *listposition;
 	char readBuffer[10];
-	 int     fd[2];
-			int pipe;
-			FILE *fp;
-	// ppid1=getppid();
-	// printf("ppid1 is %d\n",ppid1);
-	// printf("size of array: %zu\n",sizeof(listofpids)/8);
-	// sizeOfList=(sizeof(listofpids)/8);
+	int     fd[2];
+	int pipe;
+	FILE *fp;
+	char line[20];
+
 	
 	if(strcmp(argv[1],sleep1)==0){
-		printf("sleeping");
-		printf(" for %s\n",argv[2]);
-		printf("pid1 is %d\n",pid1);
+		printf("sleeping for %s\n",argv[2]);
 		//sigiot alias for SIGABRT
 		//When abort signal is received; ignore it
 		signal(SIGIOT,SIG_IGN);
 
 		pidholder=fork();
-		 // pidholder=listofpids[listposition];
-		printf("pidholder1 %d\n",pidholder);
+		printf("pidholder %d\n",pidholder);
 	
 		if(pidholder==0){
-				printf("pidholder %d",pidholder);
+				printf("pidholder child %d\n",getpid(pidholder));
 	
-				printf("\tChild prcoess created\n");
 				int n=atoi(argv[2]);
-				sleep(n);
-				int s;
-				// s=wait(&status);
-				
-				fp=fopen("pid_list","w");
-				fprintf(fp,"");
+				printf("\tChild prcoess created\n \t and is sleeping for %d seconds\n", n);
+				//Appends new process to pid list file
+				fp=fopen("pid_list","a+");
+				fprintf(fp,"%d\n",getpid(pidholder)); //writes
 				fclose(fp);
-				
+				int s=sleep(n);
+				printf("here\n");			
+				fp=fopen("pid_list","r");
+				char pidstring[10];
+				int counter=0;				
+				int oldpids[10];
+				while ( fgets( line, sizeof line, fp)){  //fgets grabs from the file stream until a \n character is reached and stores the result in line.  
+
+					sscanf(line,"%s", pid);
+					printf("pid is %s\n",pid);
+					sprintf(pidstring,"%d",getpid(pidholder));
+					printf("pidstring is %s\n",pidstring);
+					if(strncmp(line,pidstring,5)==0){
+						printf("match in strncmp\n");
+						printf("line %s\n",line);
+					}else{
+						
+						oldpids[counter]=atoi(line);
+						printf("line %d\n",atoi(line));
+						printf("oldpids at %d is %d\n",counter,oldpids[counter]);
+						counter++;
+					}
+					
+				}
+
+				// fclose(fp);
+				int i;
+				FILE *fp2;
+				fp2=fopen("pid_list","w");
+				for(i=0;i<sizeof oldpids;i++){
+					printf("old pids are %d \n",oldpids[i]);
+
+					if(oldpids[i]!=0){
+						fprintf(fp2,"%d\n",oldpids[i]);
+					}
+					
+				}
+				fclose(fp2);
 				printf("\tchild pid2 is %d\n",pid2);
 				printf("\tchilds is %d\n", s);
-
 			}
-			// printf("pidholder after if %d\n",pidholder);
-			else{
-			printf("In else\n");
-
-			printf("pidholder %d\n",pidholder);
-							// pipe = open("pids", O_WRONLY);
-							// pipe(fd);
-							// close(fd[0]);
-					FILE *fp ;
-					fp = fopen ("pid_list","a+");
-					fprintf(fp,"%d\n",getpid(pidholder)); /*writes*/ 
-					fclose(fp); 
-			// 					printf("listofpids in else:  %d\n",listofpids[0]);
-			// 			
-			// 					exit(0);
-		}
-	
-	
-	
-		// exit(0);
 	}
 
 	if(strcmp(argv[1],list)==0){
@@ -117,11 +126,17 @@ int main(int argc, char* argv[]) {
 
 	if(strcmp(argv[1],wait1)==0){
 		pid_t argPid=atoi(argv[2]);
-		printf("argPid is %i\n",argPid);
+		printf("argPid is %d\n",argPid);
 		int waitStatus;
-		waitpid(argPid,&waitStatus,WNOHANG);
-		printf("wait status %d\n",waitStatus);
-		
+		printf("pidholder %d\n",pidholder);
+		pid_t waitp=waitpid(argPid,NULL,WNOHANG);
+		printf("wait status %d\n",waitp);
+		if (waitp>0){
+			printf("greater than zero\n");			
+		}
+		else if(waitp<0){
+			printf("less than zero\n");
+		}
 	
 		
 	
@@ -146,7 +161,7 @@ int main(int argc, char* argv[]) {
 
 	}
 
-	printf("none matched");
+	printf("none matched\n");
 
 
 
