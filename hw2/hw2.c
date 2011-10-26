5f9c34dc5c7c77cfe63b1c464254a352161383c5
Rick Schmidt <rick@rickschmidt.org>
2011-10-25 21:50:34 -0500

    updatelist method added to work with kill

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -2,30 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 #include  <sys/types.h>
-#include <fcntl.h>
+// #include <fcntl.h>
 #include <signal.h>
 #include <sys/wait.h>
-
-
-void ignore(){
+char sleep1[100]="sleep";
 	
+
+
+void updateList(FILE *fp,char *line,char *pid,pid_t pidholder){
+	fp=fopen("pid_list","r");
+				char pidstring[10];
+				int counter=0;				
+				int *oldpids=malloc(1);
+				while ( fgets( line, sizeof line, fp)){  //fgets grabs from the file stream until a \n character is reached and stores the result in line.  
+
+					sscanf(line,"%s", pid);
+					printf("pid is %s\n",pid);
+					printf("pidholder is %d\n",pidholder);
+					if(pidholder==0){
+						sprintf(pidstring,"%d",getpid(pidholder));
+					}else{
+						sprintf(pidstring,"%d",pidholder);
+					}
+					printf("pidstring is %s\n",pidstring);
+					printf("line before if is %s\n\n",line);
+					if(strncmp(line,pidstring,strlen(pidstring))==0){
+						printf("match in strncmp\n");
+						printf("lineif %s\n",line);
+					}else{
+						realloc(oldpids,1+sizeof(oldpids));
+						oldpids[counter]=atoi(line);
+						printf("lineelse %d\n",atoi(line));
+						printf("oldpids at %d is %d\n",counter,oldpids[counter]);
+						counter++;
+					}
+					
+				}
+
+				// fclose(fp);
+				int i;
+				FILE *fp2;
+				fp2=fopen("pid_list","w");
+				for(i=0;i<10;i++){
+					printf("old pids are %d \n",oldpids[i]);
+
+					if(oldpids[i]!=0){
+						fprintf(fp2,"%d\n",oldpids[i]);
+
+					}
+					
+				}
+				fclose(fp2);
 }
 int main(int argc, char* argv[]) { 
-
-	printf("Cout is : %d\n",argc);
-	int i;
-
-	//print all of the varargs for testing...
-	for(i=0; i < argc; i++) printf("argv %d is: %s\t",i, argv[i]); printf("\n");
-
-
-	char sleep1[100]="sleep";
-	char list[10]="list";
+char list[10]="list";
 	char wait1[10]="wait";
 	char pid[10]="PID";
 	char pid_list[10]="PID-list";
-	char time[10]="time";
-	char kill[10]="kill";
+	char time1[10]="time";
+	char kill1[10]="kill";
 	pid_t pid1=0;
 	pid_t ppid1;
 	pid_t pid2;
@@ -39,7 +74,14 @@ int main(int argc, char* argv[]) {
 	int pipe;
 	FILE *fp;
 	char line[20];
+	printf("Cout is : %d\n",argc);
+	int i;
 
+	//print all of the varargs for testing...
+	for(i=0; i < argc; i++) printf("argv %d is: %s\t",i, argv[i]); printf("\n");
+
+
+	
 	
 	if(strcmp(argv[1],sleep1)==0){
 		printf("sleeping for %s\n",argv[2]);
@@ -61,42 +103,7 @@ int main(int argc, char* argv[]) {
 				fclose(fp);
 				int s=sleep(n);
 				printf("here\n");			
-				fp=fopen("pid_list","r");
-				char pidstring[10];
-				int counter=0;				
-				int oldpids[10];
-				while ( fgets( line, sizeof line, fp)){  //fgets grabs from the file stream until a \n character is reached and stores the result in line.  
-
-					sscanf(line,"%s", pid);
-					printf("pid is %s\n",pid);
-					sprintf(pidstring,"%d",getpid(pidholder));
-					printf("pidstring is %s\n",pidstring);
-					if(strncmp(line,pidstring,5)==0){
-						printf("match in strncmp\n");
-						printf("line %s\n",line);
-					}else{
-						
-						oldpids[counter]=atoi(line);
-						printf("line %d\n",atoi(line));
-						printf("oldpids at %d is %d\n",counter,oldpids[counter]);
-						counter++;
-					}
-					
-				}
-
-				// fclose(fp);
-				int i;
-				FILE *fp2;
-				fp2=fopen("pid_list","w");
-				for(i=0;i<10;i++){
-					printf("old pids are %d \n",oldpids[i]);
-
-					if(oldpids[i]!=0){
-						fprintf(fp2,"%d\n",oldpids[i]);
-					}
-					
-				}
-				fclose(fp2);
+				updateList(fp,line,pid,pidholder);
 				printf("\tchild pid2 is %d\n",pid2);
 				printf("\tchilds is %d\n", s);
 			}
@@ -141,16 +148,21 @@ int main(int argc, char* argv[]) {
 		printf("pid");
 
 	}
-	if(strcmp(argv[1],time)==0){
+	if(strcmp(argv[1],time1)==0){
 		if(strcmp(argv[2],pid)==0){
 			printf("time PID");
 		}
 
 	}
 
-	if(strcmp(argv[1],kill)==0){
+	if(strcmp(argv[1],kill1)==0){
 		pid_t toKill=atoi(argv[2]);
-		kill(toKill,SIGKILL);
+		int status=kill(toKill,SIGKILL);
+		FILE *fp;
+		char line[20];
+			char pid[10]="PID";
+		
+		updateList(fp,line,pid,toKill);
 
 	}
 
