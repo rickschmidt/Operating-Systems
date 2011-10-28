c2e4c623566541baf1c26ba7318526017edfbc31
Rick Schmidt <rick@rickschmidt.org>
2011-10-28 13:19:57 -0500

    wait-list still works when the first process is killed

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -17,23 +17,23 @@ void updateList(FILE *fp,char *line,char *pid,pid_t pidholder){
 				while ( fgets( line, sizeof line, fp)){  //fgets grabs from the file stream until a \n character is reached and stores the result in line.  
 
 					sscanf(line,"%s", pid);
-					printf("pid is %s\n",pid);
-					printf("pidholder is %d\n",pidholder);
+					// printf("pid is %s\n",pid);
+					// printf("pidholder is %d\n",pidholder);
 					if(pidholder==0){
 						sprintf(pidstring,"%d",getpid(pidholder));
 					}else{
 						sprintf(pidstring,"%d",pidholder);
 					}
-					printf("pidstring is %s\n",pidstring);
-					printf("line before if is %s\n\n",line);
+					// printf("pidstring is %s\n",pidstring);
+					// printf("line before if is %s\n\n",line);
 					if(strncmp(line,pidstring,strlen(pidstring))==0){
-						printf("match in strncmp\n");
-						printf("lineif %s\n",line);
+						// printf("match in strncmp\n");
+						// printf("lineif %s\n",line);
 					}else{
 						realloc(oldpids,1+sizeof(oldpids));
 						oldpids[counter]=atoi(line);
-						printf("lineelse %d\n",atoi(line));
-						printf("oldpids at %d is %d\n",counter,oldpids[counter]);
+						// printf("lineelse %d\n",atoi(line));
+						// printf("oldpids at %d is %d\n",counter,oldpids[counter]);
 						counter++;
 					}
 					
@@ -44,7 +44,7 @@ void updateList(FILE *fp,char *line,char *pid,pid_t pidholder){
 				FILE *fp2;
 				fp2=fopen("pid_list","w");
 				for(i=0;i<10;i++){
-					printf("old pids are %d \n",oldpids[i]);
+					// printf("old pids are %d \n",oldpids[i]);
 
 					if(oldpids[i]!=0){
 						fprintf(fp2,"%d\n",oldpids[i]);
@@ -78,93 +78,89 @@ char list[10]="list";
 	int pipe;
 	FILE *fp;
 	char line[20];
-	printf("Cout is : %d\n",argc);
 	int i;
 
 	//print all of the varargs for testing...
-	for(i=0; i < argc; i++) printf("argv %d is: %s\t",i, argv[i]); printf("\n");
+	// for(i=0; i < argc; i++) printf("argv %d is: %s\t\n",i, argv[i]); printf("\n");
 
 
 
 	
 	if(strcmp(argv[1],sleep1)==0){
-		printf("sleeping for %s\n",argv[2]);
-		//sigiot alias for SIGABRT
-		//When abort signal is received; ignore it
 		signal(SIGIO,SIG_IGN);	
 		signal(SIGINT,killed);
 		pidholder=fork();
-		printf("pidholder %d\n",pidholder);
 	
 		if(pidholder==0){
-				printf("pidholder child %d\n",getpid(pidholder));
 	
 				int n=atoi(argv[2]);
-				printf("\tChild prcoess created\n \t and is sleeping for %d seconds\n", n);
+				printf("\n\tChild prcoess %d created,\n\tand is sleeping for %d seconds.\n\n",getpid(pidholder), n);
 				//Appends new process to pid list file
 				fp=fopen("pid_list","a+");
 				fprintf(fp,"%d\n",getpid(pidholder)); //writes
 				fclose(fp);
 				int s=sleep(n);
-				printf("here\n");			
 				updateList(fp,line,pid,pidholder);
-				printf("\tchild pid2 is %d\n",pid2);
-				printf("\tchilds is %d\n", s);
+				printf("\tProcess %d is done sleeping.\n",getpid(pidholder));
+
+
 			}
 	}
 
 	if(strcmp(argv[1],list)==0){
-		printf("list\n");
 		fp=fopen("pid_list","r");
 		char line[100];
-			while(fgets(line, sizeof line, fp) != NULL)
-   			{
+		char lineif[100];
+		if(fgets(lineif,sizeof lineif,fp)!=NULL){
+			fclose(fp); //Close FileHandler that existed to see if the file was empty.
+			fp=fopen("pid_list","r");
+			while(fgets(line, sizeof line, fp) != NULL){
 				printf("PID: %s\n",line);
+	   		}
+			fclose(fp);	
+		}else{
+			printf("There are no currently running processes to list.\n");
+		}
 
-
-	   }
-				fclose(fp);
-		int i;
-		printf("Done printing\n");
 	}
 
 	if(strcmp(argv[1],wait1)==0){
 		pid_t argPid=atoi(argv[2]);
-		if(argv[3]!=NULL){
-			if(strcmp(argv[3],"list")){
+
+		if(argv[2]!=NULL){
+			if(strcmp(argv[2],"list")==0){
+				
 				FILE *fp;
 				fp=fopen("pid_list","r");
 				char line[10];
 				int waitStatus;
 				int i=0;
-				int * lines = NULL;
-				int *morelines;
-				printf("wait list\n");
+				char * lines = NULL;
+				char *morelines;
+				fpos_t position;
+				printf("Waiting for all processes in queue:\n Note new processes that are added after wait list are called are not waited for\n");				
 				while(fgets(line,sizeof line,fp)!=NULL){
-					i++; 
-					morelines=realloc(lines, i*sizeof(int)); 
-					lines=morelines;
-					lines[i-1] = line; 
+					pid_t linepid=atoi(line);
+					if(ferror(fp))printf("Error in stream\n");
+					while(waitStatus!=-1){						
+						waitStatus=kill(linepid,SIGIO);
 
+					}
+						fclose(fp);
+						fp=fopen("pid_list","r");
+					// rewind(fp);				
 
 				}
-				for(i=0;i< sizeof lines; i++){
-					printf("lines: %d\n",lines[i]);
-				}
-			}	
+				printf("Done waiting for all processes in queue.\n");
 		}else{
-		printf("argPid is %d\n",argPid);
 		int waitStatus;
-		// while(kill(argPid,SIGCHLD)==0){
-		// 	//works
-		// }
+		printf("Waiting for process %d...\n",argPid);
 		while(waitStatus!=-1){
 			waitStatus=kill(argPid,SIGIO);
-		
 		}
-		printf("waitStatus %d\n",waitStatus);
+		printf("Done waiting for process %d.\n",argPid);
 		}
-		
+	}
 	}
 
 	if(strcmp(argv[1],pid)==0){
@@ -219,7 +215,7 @@ char list[10]="list";
 
 	}
 
-	printf("none matched\n");
+
 
 
 
